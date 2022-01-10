9aa881f3b0ac4d7303ea4019f7cf1d466c9ceb75
EdwardGoomba <edward@dock90.io>
2022-01-09 23:14:52 -0500

    feat: move header to component

--- pages/index.js
+++ pages/index.js
@@ -1,217 +1,123 @@
-import Link from 'next/link';
-import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import Head from 'next/head';
 // components
 import CTA from '../components/cta';
 import Features from '../components/features';
 import Footer from '../components/footer';
-
-const navigation = [{ name: 'Pricing', href: '/pricing' }];
+import Header from '../components/header';
 
 export default function Home() {
   return (
-    <div className="bg-gray-50">
-      <div className="relative overflow-hidden">
-        <div className="absolute inset-y-0 h-full w-full" aria-hidden="true">
-          <div className="relative h-full">
-            <svg
-              className="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
-              width={404}
-              height={784}
-              fill="none"
-              viewBox="0 0 404 784"
-            >
-              <defs>
-                <pattern
-                  id="e229dbec-10e9-49ee-8ec3-0286ca089edf"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
+    <>
+      <Head>
+        <title>Reclerk | Background Check Management</title>
+      </Head>
+      <div className="bg-gray-50">
+        <div className="relative overflow-hidden">
+          <div className="absolute inset-y-0 h-full w-full" aria-hidden="true">
+            <div className="relative h-full">
+              <svg
+                className="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
                 width={404}
                 height={784}
-                fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)"
-              />
-            </svg>
-            <svg
-              className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
-              width={404}
-              height={784}
-              fill="none"
-              viewBox="0 0 404 784"
-            >
-              <defs>
-                <pattern
-                  id="d2a68204-c383-44b1-b99f-42ccff4e5365"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={784}
-                fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)"
-              />
-            </svg>
-          </div>
-        </div>
-
-        <div className="relative pt-6 pb-16 sm:pb-24">
-          <Popover>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
-              <nav
-                className="relative flex items-center justify-between sm:h-10 md:justify-center"
-                aria-label="Global"
+                fill="none"
+                viewBox="0 0 404 784"
               >
-                <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <Link href="/">
-                      <a>
-                        <span className="sr-only">Reclerk</span>
-                        <img
-                          className="h-8 w-auto sm:h-10"
-                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                          alt=""
-                        />
-                      </a>
-                    </Link>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Open main menu</span>
-                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden md:flex md:space-x-10">
-                  {navigation.map((item) => (
-                    <Link href={item.href} key={item.name}>
-                      <a className="font-medium text-emerald-800 hover:text-emerald-700">
-                        {item.name}
-                      </a>
-                    </Link>
-                  ))}
-                </div>
-                <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                  <span className="inline-flex rounded-md shadow">
-                    <Link href="/signup">
-                      <a className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-emerald-800 bg-white hover:text-emerald-700">
-                        Sign up
-                      </a>
-                    </Link>
+                <defs>
+                  <pattern
+                    id="e229dbec-10e9-49ee-8ec3-0286ca089edf"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={784}
+                  fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)"
+                />
+              </svg>
+              <svg
+                className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 0 404 784"
+              >
+                <defs>
+                  <pattern
+                    id="d2a68204-c383-44b1-b99f-42ccff4e5365"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={784}
+                  fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)"
+                />
+              </svg>
+            </div>
+          </div>
+
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <Header />
+            <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+              <div className="text-center">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block">
+                    Avoid commercial project delays due to outdated background
+                    checks.
                   </span>
-                </div>
-              </nav>
+                </h1>
+                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                  Our tool visually highlights expiring background checks and
+                  makes document management simple.
+                </p>
+              </div>
             </div>
+          </div>
 
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
-                    {navigation.map((item) => (
-                      <Link key={item.name} href={item.href}>
-                        <a className="block px-3 py-2 rounded-md text-base font-medium text-emerald-800 hover:text-emerald-700 hover:bg-gray-50">
-                          {item.name}
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
-                  <Link href="/signup">
-                    <a className="block w-full px-5 py-3 text-center font-medium text-emerald-800 bg-gray-50 hover:bg-gray-100 hover:text-emerald-700">
-                      Sign up
-                    </a>
-                  </Link>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
-            <div className="text-center">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block">
-                  Avoid commercial project delays due to outdated background
-                  checks.
-                </span>
-              </h1>
-              <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                Our tool visually highlights expiring background checks and
-                makes document management simple.
-              </p>
+          <div className="relative">
+            <div className="absolute inset-0 flex flex-col" aria-hidden="true">
+              <div className="flex-1" />
+              <div className="flex-1 w-full bg-gray-800" />
+            </div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+              <img
+                className="relative rounded-lg shadow-lg"
+                src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
+                alt="App screenshot"
+              />
             </div>
           </div>
         </div>
-
-        <div className="relative">
-          <div className="absolute inset-0 flex flex-col" aria-hidden="true">
-            <div className="flex-1" />
-            <div className="flex-1 w-full bg-gray-800" />
-          </div>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6">
-            <img
-              className="relative rounded-lg shadow-lg"
-              src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
-              alt="App screenshot"
-            />
-          </div>
-        </div>
-      </div>
-      {/* social proof */}
-      <div className="bg-gray-800">
-        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          {/* <h2 className="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">
+        {/* social proof */}
+        <div className="bg-gray-800">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            {/* <h2 className="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">
             Trusted by over 26,000 forward-thinking companies
           </h2>
           <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
@@ -251,12 +157,13 @@ export default function Home() {
               />
             </div>
           </div> */}
+          </div>
         </div>
+        <CTA />
+        <Features />
+        <CTA />
+        <Footer />
       </div>
-      <CTA />
-      <Features />
-      <CTA />
-      <Footer />
-    </div>
+    </>
   );
 }
