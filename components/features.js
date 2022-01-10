ee8f0f5e1375603b34374a92eacb0de53bbf624e
EdwardGoomba <edward@dock90.io>
2022-01-09 22:43:23 -0500

    feat: layout homepage

---
+++ components/features.js
@@ -0,0 +1,89 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, MailIcon, ScaleIcon } from '@heroicons/react/outline'
+
+const Features = () => {
+  return (
+    <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
+      <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+
+        <div className="relative">
+          <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            A better way to send money
+          </h2>
+          <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in,
+            accusamus quisquam.
+          </p>
+        </div>
+
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div className="relative">
+            <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+              Transfer funds world-wide
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores
+              officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere.
+              Totam, velit.
+            </p>
+          </div>
+
+          <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+            <img
+              className="relative mx-auto"
+              width={490}
+              src="https://tailwindui.com/img/features/feature-example-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">Always in the loop</h3>
+              <p className="mt-3 text-lg text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex obcaecati natus eligendi delectus,
+                cum deleniti sunt in labore nihil quod quibusdam expedita nemo.
+              </p>
+            </div>
+
+            <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+              <img
+                className="relative mx-auto"
+                width={490}
+                src="https://tailwindui.com/img/features/feature-example-2.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div className="relative">
+            <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+              Transfer funds world-wide
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores
+              officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere.
+              Totam, velit.
+            </p>
+          </div>
+
+          <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+            <img
+              className="relative mx-auto"
+              width={490}
+              src="https://tailwindui.com/img/features/feature-example-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Features
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import Link from 'next/link';
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
+// components
+import CTA from '../components/cta';
+import Features from '../components/features';
+import Footer from '../components/footer';
 
 const navigation = [{ name: 'Pricing', href: '/pricing' }];
 
@@ -177,13 +181,10 @@ export default function Home() {
           <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
             <div className="text-center">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block">Data to enrich your</span>
-                <span className="block text-indigo-600">online business</span>
+                <span className="block">Avoid commercial project delays due to outdated background checks.</span>
               </h1>
               <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                Our tool visually highlights expiring background checks and makes document management simple.
               </p>
             </div>
           </div>
@@ -203,9 +204,10 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* social proof */}
       <div className="bg-gray-800">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <h2 className="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">
+          {/* <h2 className="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">
             Trusted by over 26,000 forward-thinking companies
           </h2>
           <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
@@ -244,9 +246,13 @@ export default function Home() {
                 alt="Workcation"
               />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
+      <CTA />
+      <Features />
+      <CTA />
+      <Footer />
     </div>
   );
 }
